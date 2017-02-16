5988424dead5b4cea28f9eaebacf8839abd5b0f6
jonatansalas <jonatansalas@live.com.ar>
2017-02-16 11:43:55 -0300

    Update Example

--- src/LinkButtonContainer.js
+++ src/LinkButtonContainer.js
@@ -4,9 +4,18 @@ import LinkButton from './LinkButton';
 class LinkButtonContainer extends React.Component {
     render() {
         return (
-            <LinkButton href="/properties" flat primary>
-                Touch me!
-            </LinkButton>
+            <div>
+                <div>
+                    <LinkButton href="/properties" flat primary>
+                        Touch me!
+                    </LinkButton>
+                </div>
+                <div>
+                    <LinkButton href="/properties" flat={false} primary>
+                        Touch me!
+                    </LinkButton>
+                </div>
+            </div>
         );
     }
 }
--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import { Router, Route, browserHistory } from 'react-router';
 import LinkButtonContainer from './LinkButtonContainer.js';
-import RaisedLinkButtonContainer from './RaisedLinkButtonContainer.js';
 
 injectTapEventPlugin();
 
@@ -16,7 +15,6 @@ const App = () => (
     <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
         <Router history={browserHistory}>
             <Route path='/' component={LinkButtonContainer}/>
-            <Route path='/raised' component={RaisedLinkButtonContainer}/>
             <Route path='/properties' component={HelloProperties}/>
         </Router>
     </MuiThemeProvider>
